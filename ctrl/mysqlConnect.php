18f5747a7b7b96d540416548740b78099d6fe619
Levani Beroshvili <Levani Beroshvili>
2016-10-25 21:08:14 +0400

    add Mysql to error connecting to mysql returns

--- ctrl/mysqlConnect.php
+++ ctrl/mysqlConnect.php
@@ -7,7 +7,7 @@
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+	    die("Mysql connection failed: " . $conn->connect_error);
 	}
 
 ?>
