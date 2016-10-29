875e2602c34e9e94fd24c0b6c3e038bca403e186
Levani Beroshvili <Levani Beroshvili>
2016-10-29 10:01:04 +0400

    change MYSQL server

--- ctrl/mysqlConnect.php
+++ ctrl/mysqlConnect.php
@@ -1,8 +1,8 @@
 <?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "test1";
+	$servername = "eu-cdbr-west-01.cleardb.com";
+	$username = "b5c03625a28a33";
+	$password = "bb060872";
+	$dbname = "heroku_c75925a870861fd";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
