6e633cb666380d58a8e197e36c4ed2a70c559ad8
levani132 <lberoshvili9@gmail.com>
2016-11-05 16:37:01 +0400

    fix url 2

--- ctrl/login.php
+++ ctrl/login.php
@@ -10,15 +10,15 @@
 	$password = mysqli_query($conn, "SELECT `Password` FROM `admins` WHERE `Username` = '".$_POST['username']."'");	
 	$result = mysqli_fetch_assoc($password);
 
+	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 
 	if(mysqli_num_rows($password) && $result["Password"] == md5($_POST['password'])){
 		$_SESSION['started'] = 1;
 		$_SESSION['time'] = time();
-		header('Location: http://localhost:8080/ctrl/');
+		header('Location: http://$uri/ctrl/');
 	}
 	else{
 		$err = "Please type correct username/password.";
-		$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		header('Location: http://$uri/ctrl/?err=1');
 	}
 ?>
