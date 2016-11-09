2d432905aa4c78d3334d82e1e6f50f013bc5efb9
levani132 <lberoshvili9@gmail.com>
2016-11-09 15:52:24 +0400

    something

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php 
+	error_reporting(0);
 	$servername = "eu-cdbr-west-01.cleardb.com";
 	$username = "b5c03625a28a33";
 	$password = "bb060872";
 	$dbname = "heroku_c75925a870861fd";
-
-	// Create connection
-	$db = new mysqli($servername, $username, $password, $dbname);
-	if ($db->connect_error) {
-	    die("Mysql connection failed: " . $db->connect_error);
-	}
+	$encode = "UTF8";
+	//-----------------------------------
+	//mysql dakavshireba
+	$db = mysqli_connect($servername,$username,$password);
+	mysqli_select_db($db,$dbname) or die (mysqli_error($db));
+	mysqli_query($db,"SET NAMES '".$encode."'");
 
 
 	$uri   = rtrim($_SERVER['SERVER_NAME'], '/\\');
