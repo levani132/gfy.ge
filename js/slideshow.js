fccc828b7f5acf201c1001f87fabb779a6d6adf6
levani132 <lberoshvili9@gmail.com>
2016-11-09 15:08:25 +0400

    something

--- js/slideshow.js
+++ js/slideshow.js
@@ -8,6 +8,7 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    if(x.length < 1) return;
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
